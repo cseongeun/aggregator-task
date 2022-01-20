c4e598b5c6c70cf9d9348b6d89f342f06a045a62
eun <eun@hexlant.com>
2022-01-20 17:34:50 +0900

    .

--- src/event-interaction/event-interaction.all.migration.task.ts
+++ src/event-interaction/event-interaction.all.migration.task.ts
@@ -2,29 +2,74 @@ import { Injectable } from '@nestjs/common';
 import { TASK_ID } from '../app.constant';
 import { HandlerService } from '../app/handler/handler.service';
 import { TaskBase } from '../task.base';
-import { RedisService, REDIS_CACHE_KEY } from '@seongeun/aggregator-common';
+import { RedisService, REDIS_KEY } from '@seongeun/aggregator-common';
+import {
+  EventService,
+  InteractionService,
+} from '@seongeun/aggregator-base/lib/service';
+import { isGreaterThan } from '@seongeun/aggregator-util/lib/bignumber';
 
 @Injectable()
 export class EventInteractionAllMigrationTask extends TaskBase {
-  loggingForm(): Record<string, any> {
-    throw new Error('Method not implemented.');
-  }
-  process(data: any): Promise<Record<string, any>> {
-    throw new Error('Method not implemented.');
-  }
-
   constructor(
     public readonly handlerService: HandlerService,
     public readonly redisService: RedisService,
+    public readonly eventService: EventService,
+    public readonly interactionService: InteractionService,
   ) {
     super(TASK_ID.EVENT_INTERACTION_ALL_MIGRATION, handlerService);
   }
 
-  async run(): Promise<Record<string, any>> {
-    const result = await this.redisService.getData(
-      REDIS_CACHE_KEY.EVENT_STORAGE,
-    );
-    console.log('here', JSON.parse(result));
+  loggingForm(): Record<string, any> {
+    return {
+      event: 0,
+      interaction: 0,
+    };
+  }
+
+  async popTotalData(key: REDIS_KEY): Promise<string[]> {
+    const len = await this.redisService.getListLen(key);
+
+    if (!isGreaterThan(len, 0)) {
+      return [];
+    }
+
+    return this.redisService.popListData(key, len);
+  }
+
+  async process(data: {
+    eventData;
+    interactionData;
+  }): Promise<Record<string, any>> {
+    const { eventData, interactionData } = data;
+
+    if (isGreaterThan(eventData.length, 0)) {
+      await this.eventService.repository.createAllIfNotExistBy(
+        eventData.map((data) => JSON.parse(data)),
+      );
+    }
+
+    if (isGreaterThan(interactionData.length, 0)) {
+      await this.interactionService.repository.createAllIfNotExistBy(
+        interactionData.map((data) => JSON.parse(data)),
+      );
+    }
+
     return {};
+  }
+
+  async run(): Promise<Record<string, any>> {
+    try {
+      const [eventData, interactionData] = await Promise.all([
+        this.popTotalData(REDIS_KEY.EVENT_STORAGE),
+        this.popTotalData(REDIS_KEY.INTERACTION_STORAGE),
+      ]);
+
+      await this.process({ eventData, interactionData });
+
+      return { event: eventData.length, interaction: interactionData.length };
+    } catch (e) {
+      throw Error(e);
+    }
   }
 }
--- src/event-interaction/event-interaction.task.module.ts
+++ src/event-interaction/event-interaction.task.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import {
+  EventModule,
+  InteractionModule,
+} from '@seongeun/aggregator-base/lib/module';
 import { RedisModule } from '@seongeun/aggregator-common';
 import { HandlerModule } from '../app/handler/handler.module';
 import { EventInteractionAllMigrationTask } from './event-interaction.all.migration.task';
+
 @Module({
-  imports: [HandlerModule, RedisModule],
+  imports: [HandlerModule, RedisModule, EventModule, InteractionModule],
   providers: [EventInteractionAllMigrationTask],
   exports: [EventInteractionAllMigrationTask],
 })
